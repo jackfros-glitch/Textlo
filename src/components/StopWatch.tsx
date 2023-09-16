856f6c064bda265ee7b4690165a4a5362a8f3267
unknown <hirekaanm3@gmail.com>
2023-09-16 23:29:40 +0100

    feat: stop watch component Add the functionality

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,21 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-const StopWatch = () => {
-  // state to store time
-  const [time, setTime] = useState(0);
+interface Props {
+  time: number;
+}
 
-  // state to check stopwatch running or not
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    let intervalId: number;
-    if (isRunning) {
-      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
-      intervalId = setInterval(() => setTime(time + 1), 10);
-    }
-    return () => clearInterval(intervalId);
-  }, [isRunning, time]);
-
+const StopWatch = ({ time }: Props) => {
   // Hours calculation
   const hours = Math.floor(time / 360000);
 
@@ -25,15 +14,6 @@ const StopWatch = () => {
   // Seconds calculation
   const seconds = Math.floor((time % 6000) / 100);
 
-  // Method to start and stop timer
-  const startAndPause = () => {
-    setIsRunning(!isRunning);
-  };
-
-  // Method to reset timer back to 0
-  const reset = () => {
-    setTime(0);
-  };
   return (
     <div className="stopwatch-container">
       <p className="stopwatch-time">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 import { faStop } from "@fortawesome/free-solid-svg-icons/faStop";
@@ -6,20 +6,61 @@ import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons/faEllipsis
 import StopWatch from "../components/StopWatch";
 
 const Home = () => {
+  // state to store time
+  const [time, setTime] = useState(0);
+
+  // state to check stopwatch running or not
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId: number;
+    if (isRunning) {
+      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+  // Method to start and stop timer
+  const startAndPause = () => {
+    setIsRunning(!isRunning);
+  };
+
+  // Method to reset timer back to 0
+  const reset = () => {
+    setTime(0);
+  };
   return (
     <>
       <div>
         <div className="container">
-          <StopWatch />
+          <StopWatch time={time} />
           <div className="microphone-container">
-            <button className="mx-2 mr-3">
-              <FontAwesomeIcon
-                icon={faMicrophone}
-                size="2xl"
-                style={{ color: "blue" }}
-              />
+            <button
+              id="startPauseButton"
+              className="mx-2 mr-3"
+              onClick={startAndPause}
+            >
+              {isRunning ? (
+                <FontAwesomeIcon
+                  icon={faMicrophone}
+                  size="2xl"
+                  style={{ color: "blue" }}
+                  beatFade
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faMicrophone}
+                  size="2xl"
+                  style={{ color: "blue" }}
+                />
+              )}
             </button>
-            <button className="mx-2 border border-blue-200 p-2 py-0 flex h-8 mr-3 items-center">
+            <button
+              id="stopButton"
+              className="mx-2 border border-blue-200 p-2 py-0 flex h-8 mr-3 items-center"
+              onClick={reset}
+            >
               <FontAwesomeIcon icon={faStop} style={{ color: "red" }} />
             </button>
             <button className="mx-2">
