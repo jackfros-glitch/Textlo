7c40c9d423560f98d2dda8dd625671017a9a8bcc
unknown <hirekaanm3@gmail.com>
2023-09-16 23:01:56 +0100

    feat: stop watch component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,28 +3,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 import { faStop } from "@fortawesome/free-solid-svg-icons/faStop";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons/faEllipsisVertical";
+import StopWatch from "../components/StopWatch";
 
 const Home = () => {
   return (
     <>
       <div>
-        <div className="microphone-container">
-          <button className="mx-2 mr-3">
-            <FontAwesomeIcon
-              icon={faMicrophone}
-              size="2xl"
-              style={{ color: "blue" }}
-            />
-          </button>
-          <button className="mx-2  mr-3">
-            <FontAwesomeIcon icon={faStop} style={{ color: "blue" }} />
-          </button>
-          <button className="mx-2">
-            <FontAwesomeIcon
-              icon={faEllipsisVertical}
-              style={{ color: "blue" }}
-            />
-          </button>
+        <div className="container">
+          <StopWatch />
+          <div className="microphone-container">
+            <button className="mx-2 mr-3">
+              <FontAwesomeIcon
+                icon={faMicrophone}
+                size="2xl"
+                style={{ color: "blue" }}
+              />
+            </button>
+            <button className="mx-2 border border-blue-200 p-2 py-0 flex h-8 mr-3 items-center">
+              <FontAwesomeIcon icon={faStop} style={{ color: "red" }} />
+            </button>
+            <button className="mx-2">
+              <FontAwesomeIcon
+                icon={faEllipsisVertical}
+                style={{ color: "blue" }}
+              />
+            </button>
+          </div>
         </div>
       </div>
     </>
