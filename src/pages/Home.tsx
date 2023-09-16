e6b20a1c299a78f47ad354fc898334a2a23ab424
unknown <hirekaanm3@gmail.com>
2023-09-16 18:57:57 +0100

    feat: microphone container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import { faStop } from "@fortawesome/free-solid-svg-icons/faStop";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons/faEllipsisVertical";
 
 const Home = () => {
-  return <div className="font-bold m-auto w-fit">Home </div>;
+  return (
+    <>
+      <div>
+        <div className="microphone-container">
+          <button className="mx-2 mr-3">
+            <FontAwesomeIcon
+              icon={faMicrophone}
+              size="2xl"
+              style={{ color: "blue" }}
+            />
+          </button>
+          <button className="mx-2  mr-3">
+            <FontAwesomeIcon icon={faStop} style={{ color: "blue" }} />
+          </button>
+          <button className="mx-2">
+            <FontAwesomeIcon
+              icon={faEllipsisVertical}
+              style={{ color: "blue" }}
+            />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
